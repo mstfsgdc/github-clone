7170549efa2968496d989ea23cecccee2a131ddf
Mustafa Sagdic <mmsagdic@gmail.com>
2022-10-16 03:19:50 +0300

    UPDATE routes/HomePage.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,21 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
     return (
         <header className="h-[3.75rem] bg-[#161B22] px-[1.5rem] py-[1rem] flex items-center justify-between">
-            <div className="left-panel flex items-center">
+            <div className="flex items-center left-panel">
                 <FontAwesomeIcon icon={faGithub} color={'#FFFFFFB3'} size={'2x'} />
                 <input type="text" name="search" id="search" placeholder={'Search or jump to...'} className='text-inherit bg-[#0d1117] w-64 py-1 px-[0.75rem] text-[0.875rem] rounded border border-[#30363d] ml-4 focus:outline-0 active:outline-0 text-white' />
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" aria-hidden="true" className="relative -left-7"><path fill="none" stroke="#979A9C" opacity=".4" d="M3.5.5h12c1.7 0 3 1.3 3 3v13c0 1.7-1.3 3-3 3h-12c-1.7 0-3-1.3-3-3v-13c0-1.7 1.3-3 3-3z"></path><path fill="#979A9C" d="M11.8 6L8 15.1h-.9L10.8 6h1z"></path></svg>
-                <a className='font-[600] text-[#f0f6fc] ml-4 whitespace-nowrap text-[0.875rem]'>Pulls</a>
-                <a className='font-[600] text-[#f0f6fc] ml-4 whitespace-nowrap text-[0.875rem]'>Issues</a>
-                <a className='font-[600] text-[#f0f6fc] ml-4 whitespace-nowrap text-[0.875rem]'>Marketplace</a>
-                <a className='font-[600] text-[#f0f6fc] ml-4 whitespace-nowrap text-[0.875rem]'>Explore</a>
+                <a className='font-[600] text-primary-500 ml-4 whitespace-nowrap text-[0.875rem]'>Pulls</a>
+                <a className='font-[600] text-primary-500 ml-4 whitespace-nowrap text-[0.875rem]'>Issues</a>
+                <a className='font-[600] text-primary-500 ml-4 whitespace-nowrap text-[0.875rem]'>Marketplace</a>
+                <a className='font-[600] text-primary-500 ml-4 whitespace-nowrap text-[0.875rem]'>Explore</a>
             </div>
-            <div className="right-panel">
+            <div className="right-panel flex justify-between w-20">
                 <FontAwesomeIcon icon={faBell} color={'#f0f6fc'} />
-                <FontAwesomeIcon icon={faPlus} color={'#f0f6fc'} className='ml-4' />
+                <FontAwesomeIcon icon={faPlus} color={'#f0f6fc'} />
+                <div className="avatar w-5 h-5">
+                    <img src="https://avatars.githubusercontent.com/u/17967909?s=40&v=4" alt="avatar" className='rounded-full' />
+                </div>
             </div>
         </header>
     )
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,6 +1,20 @@
 const HomePage = () => {
     return (
-        <h1>Hi Twitch</h1>
+        <div className="flex flex-row overflow-auto">
+            <div className="drawer bg-bg_black-500 w-1/3 min-h-screen border-r border-[#21262d] text-primary-500">
+                <div className="dashboard sticky">
+                    Testt
+                </div>
+            </div>
+            <main className="w-full h-full flex flex-row bg-lightblack-500">
+                <div className="content w-full h-screen text-primary-500 mt-4 mx-8">
+                    Content
+                </div>
+                <div className="sidebar w-1/2 h-screen text-primary-500 mt-8">
+                    Sidebar
+                </div>
+            </main>
+        </div>
     )
 }
 
